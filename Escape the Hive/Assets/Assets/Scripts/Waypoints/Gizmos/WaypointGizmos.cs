e22bcaaa25efe451ff0b6c1a82a9a2e61822cf3e
TimothyOlenio <52928854+TimothyOlenio@users.noreply.github.com>
2019-11-12 00:22:09 -0500

    Created Waypoints and Gizmo
    Created all the waypoints, unfortunatly, I created them while in play mode... ugh!!!!!

---
+++ Escape the Hive/Assets/Assets/Scripts/Waypoints/Gizmos/WaypointGizmos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointGizmos : MonoBehaviour
+{
+    public float size;
+    private Transform[] waypoints;
+    void OnDrawGizmos()
+    {
+        waypoints = gameObject.GetComponentsInChildren<Transform>();
+        for (int i = 1; i < waypoints.Length; i++)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(waypoints[i].position, size);
+        }
+    }
+}
