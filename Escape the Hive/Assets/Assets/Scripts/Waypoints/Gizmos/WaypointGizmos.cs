c8b346940c1b3137ae95b4f646e95d3882a4a82c
TimothyOlenio <52928854+TimothyOlenio@users.noreply.github.com>
2019-11-12 16:05:12 -0500

    Redid the waypoints

--- Escape the Hive/Assets/Assets/Scripts/Waypoints/Gizmos/WaypointGizmos.cs
+++ Escape the Hive/Assets/Assets/Scripts/Waypoints/Gizmos/WaypointGizmos.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class WaypointGizmos : MonoBehaviour
 {
+    //Create Variables
+    //A float that controls the size of the object
     public float size;
+    //Create an array of transform components called waypoints
     private Transform[] waypoints;
+    
+    //This function creates a sphere aand colours it yellow around the objects, then draw a line between them1
     void OnDrawGizmos()
     {
+        //set the waypoints to the children of the attached object
         waypoints = gameObject.GetComponentsInChildren<Transform>();
+        //Create a Vector3  that holds the last position of the previous waypoint
+        Vector3 last = waypoints[waypoints.Length - 1].position;
+        //Cycle through the waypoints array and draw a sphere around them and colour them yellow
         for (int i = 1; i < waypoints.Length; i++)
         {
             Gizmos.color = Color.yellow;
             Gizmos.DrawSphere(waypoints[i].position, size);
+            Gizmos.DrawLine(last, waypoints[i].position);
+
+            last = waypoints[i].position;
         }
     }
 }
