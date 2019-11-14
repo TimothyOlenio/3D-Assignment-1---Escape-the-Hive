52299f67f4bfef4f7abbe728e2b3d57d479568ed
TimothyOlenio <52928854+TimothyOlenio@users.noreply.github.com>
2019-11-14 16:15:07 -0500

    Added AI car

--- Escape the Hive/Assets/Assets/Scripts/AI/CarController/CarController.cs
+++ Escape the Hive/Assets/Assets/Scripts/AI/CarController/CarController.cs
@@ -2,51 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Car : MonoBehaviour
+public class CarController : MonoBehaviour
 {
-    //Create Variables
-    //The speed at which the wheels turn the car
-    public float maxTurnAngle;
-    //The speed that the wheels push the car forward
-    public float maxTorque;
-    //Create collider variables to attach to WheelColliders
+    public float maxTurnAngle = 10;
+    public float maxTorque = 10;
     public WheelCollider WheelFront_Left;
     public WheelCollider WheelFront_Right;
     public WheelCollider WheelBack_Left;
     public WheelCollider WheelBack_Right;
 
-    //Create transform colliders to attach the Rendered Wheels 
     public Transform WheelTransformFL;
     public Transform WheelTransformFR;
     public Transform WheelTransformBL;
     public Transform WheelTransformBR;
 
-    //A vector that pushes the car down to prevent it from flying away
     public Vector3 centerOfMassAdjustment = new Vector3(0f, -0.9f, 0f);
     private Rigidbody body;
 
-    //A 
     public float spoilerRatio = 0.1f;
-    //while the gas is off, the car slows down by this rate
-    public float decelerationTorque;
 
-    //the car's maximum speed
+    public float decelerationTorque = 30;
+
     public float topSpeed = 150;
-    //A variable to get the player's current movement speed
+    public float maxReverseSpeed = -50;
     private float currentSpeed;
 
-    //when the brakes are hit, slow down the car by this rate
     public float maxBrakeTorque = 100;
     private bool applyHandBrake = false;
 
     public float handBrakeForwardSlip = 0.04f;
     public float handBrakeSidewaysSlip = 0.08f;
 
+    public Transform WaypointContainer;
+
+    private Transform[] waypoints;
+    private int currentWaypoint = 0;
+    private float inputSteer;
+    private float inputTorque;
+
+    public float brakeingDistance = 6f;
+    public float forwardOffset;
 
     private void Start()
     {
         body = GetComponent<Rigidbody>();
         body.centerOfMass += centerOfMassAdjustment;
+
+        //Get the waypoints from the track.
+        GetWaypoints();
     }
 
     private void Update()
@@ -64,84 +67,75 @@ public class Car : MonoBehaviour
     void FixedUpdate()
     {
         //Front wheel steering
-        WheelFront_Left.steerAngle = Input.GetAxis("Horizontal") * maxTurnAngle;
-        WheelFront_Right.steerAngle = Input.GetAxis("Horizontal") * maxTurnAngle;
-
+        Vector3 RelativeWaypointPosition = transform.InverseTransformPoint(new Vector3(waypoints[currentWaypoint].position.x, transform.position.y, waypoints[currentWaypoint].position.z));
+        inputSteer = RelativeWaypointPosition.x / RelativeWaypointPosition.magnitude;
 
         Vector3 LocalVelocity = transform.InverseTransformDirection(body.velocity);
         body.AddForce(-transform.up * (LocalVelocity.z * spoilerRatio), ForceMode.Impulse);
 
 
+        if (Mathf.Abs(inputSteer) < 0.5f)
+        {
+            inputTorque = (RelativeWaypointPosition.z / RelativeWaypointPosition.magnitude);
+            applyHandBrake = false;
+        }
+        else
+        {
+            if (body.velocity.magnitude > 10)
+            {
+                applyHandBrake = true;
+            }
+            else if (LocalVelocity.z < 0)
+            {
+                applyHandBrake = false;
+                inputTorque = -1;
+                inputSteer *= -1;
+            }
+            else
+            {
+                applyHandBrake = false;
+                inputTorque = 0;
+            }
+        }
+        //Hand brake
+        if (applyHandBrake)
+        {
+            SetSlipValues(handBrakeForwardSlip, handBrakeSidewaysSlip);
+        }
+        else
+        {
+            SetSlipValues(1f, 1f);
+        }
+
+        if (RelativeWaypointPosition.magnitude < 25)
+        {
+            currentWaypoint++;
+            if (currentWaypoint >= waypoints.Length)
+            {
+                currentWaypoint = 0;
+                //RaceManager.Instance.LapFinishedByAI(this);
+            }
+        }
+
+        WheelFront_Left.steerAngle = inputSteer * maxTurnAngle;
+        WheelFront_Right.steerAngle = inputSteer * maxTurnAngle;
+
 
         //  KM/H
-        currentSpeed = WheelBack_Left.radius * WheelBack_Right.rpm * Mathf.PI * 0.12f;
-
-
-
-        print(currentSpeed);
-        print("Radius " + WheelBack_Left.radius);
-        print("RPM " + WheelBack_Right.rpm);
-        print("PIE " + Mathf.PI);
-        print("" + 0.12f);
-
-
-
-        if (currentSpeed < topSpeed)
+        currentSpeed = WheelBack_Left.radius * WheelBack_Left.rpm * Mathf.PI * 0.12f;
+        if (currentSpeed < topSpeed && currentSpeed > maxReverseSpeed)
         {
+            float adjustment = ForwardRayCast();
+
             //Rear wheel drive
-            WheelBack_Left.motorTorque = Input.GetAxis("Vertical") * maxTorque;
-            WheelBack_Right.motorTorque = Input.GetAxis("Vertical") * maxTorque;
-            print(currentSpeed);
+            WheelBack_Left.motorTorque = inputTorque * adjustment * maxTorque;
+            WheelBack_Right.motorTorque = inputTorque * adjustment * maxTorque;
         }
         else
         {
             WheelBack_Left.motorTorque = 0;
             WheelBack_Right.motorTorque = 0;
         }
-        //Hand brake
-
-        if(Input.GetButton("Jump"))
-        {
-            applyHandBrake = true;
-            WheelFront_Left.brakeTorque = maxBrakeTorque;
-            WheelFront_Right.brakeTorque = maxBrakeTorque;
-
-            //PowerSlide
-            if(GetComponent<Rigidbody>().velocity.magnitude > 1)
-            {
-                SetSlipValues(handBrakeForwardSlip, handBrakeSidewaysSlip);
-            }
-            else
-            {
-                SetSlipValues(1f, 1f);
-            }
-
-        }
-        else
-        {
-            applyHandBrake = false;
-            WheelFront_Left.brakeTorque = 0;
-            WheelFront_Right.brakeTorque = 0;
-            SetSlipValues(1f, 1f);
-        }
-
-        if (!applyHandBrake && ((Input.GetAxis("Vertical") <= -0.5f && LocalVelocity.z > 0) ||
-            (Input.GetAxis("Vertical") <= -0.5f && LocalVelocity.z > 0)))
-        {
-            WheelBack_Left.brakeTorque = decelerationTorque + maxTorque;
-            WheelBack_Right.brakeTorque = decelerationTorque + maxTorque;
-        }
-        else if (!applyHandBrake && Input.GetAxis("Vertical") == 0)
-        {
-            WheelBack_Left.brakeTorque = decelerationTorque;
-            WheelBack_Right.brakeTorque = decelerationTorque;
-        }
-        else
-        {
-            WheelBack_Left.brakeTorque = 0;
-            WheelBack_Right.brakeTorque = 0;
-        }
-
 
     }
 
@@ -168,7 +162,7 @@ public class Car : MonoBehaviour
     {
         WheelHit Contact = new WheelHit();
 
-        if(WheelFront_Left.GetGroundHit(out Contact))
+        if (WheelFront_Left.GetGroundHit(out Contact))
         {
             Vector3 temp = WheelFront_Left.transform.position;
             temp.y = (Contact.point + (WheelFront_Left.transform.up * WheelFront_Left.radius)).y;
@@ -195,6 +189,45 @@ public class Car : MonoBehaviour
             WheelTransformBL.position = temp;
 
         }
+    }
+
+    float ForwardRayCast()
+    {
+        RaycastHit hit;
+        Vector3 CarFront = transform.position + (transform.forward * forwardOffset);
+        Debug.DrawRay(CarFront, transform.forward * brakeingDistance);
+
+        if (Physics.Raycast(CarFront, transform.forward, out hit, brakeingDistance))
+        {
+            return (((CarFront = hit.point).magnitude / brakeingDistance) * 2) - 1;
+        }
+
+        return 1f;
+    }
+
+    void GetWaypoints()
+    {
+        Transform[] potentialWaypoints = WaypointContainer.GetComponentsInChildren<Transform>();
+        waypoints = new Transform[(potentialWaypoints.Length - 1)];
+
+        for (int i = 1; i < potentialWaypoints.Length; i++)
+        {
+            waypoints[i - 1] = potentialWaypoints[i];
+        }
+    }
+    public Transform GetCurrentWaypoint()
+    {
+        return waypoints[currentWaypoint];
+    }
+    public Transform GetLastWaypoint()
+    {
+        if (currentWaypoint - 1 < 0)
+        {
+            return waypoints[waypoints.Length - 1];
+        }
+
+        return waypoints[currentWaypoint - 1];
+
     }
 
 }
